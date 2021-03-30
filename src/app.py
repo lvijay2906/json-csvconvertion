4dffe6ed74f8064e56d8a45fcde2fb6e4bad9e44
lvijay2906 <81562832+lvijay2906@users.noreply.github.com>
2021-03-30 09:40:52 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello World"
-
-    if __name__ == "__main__":
-        app.run()
+import pandas as pd
+df = pd.read_json (r'.\json\orders_data.json')
+df.to_csv (r'.\csv\data_file.csv', index = None)
