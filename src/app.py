0900db2b02950dbe940e43637f502d80a256ae29
lvijay2906 <81562832+lvijay2906@users.noreply.github.com>
2021-03-30 09:41:51 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df = pd.read_json (r'.\json\orders_data.json')
-df.to_csv (r'.\csv\data_file.csv', index = None)
+df = pd.read_json (r'.\orders_data.json')
+df.to_csv (r'.\data_file.csv', index = None)
